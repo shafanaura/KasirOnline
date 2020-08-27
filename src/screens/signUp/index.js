81256e21b546ccb8473e716a9703cc310954648c
shafanaura <shafanaura48@gmail.com>
2020-08-27 11:14:07 +0700

    FIX : Responsive Screens

--- src/screens/signUp/index.js
+++ src/screens/signUp/index.js
@@ -1,55 +1,61 @@
 import React from 'react';
-import {View, ScrollView} from 'react-native';
+import {View, ScrollView, StyleSheet} from 'react-native';
 import {Button, Icon, Input} from '@ui-kitten/components';
 
-import styles from './styles';
 import AvatarsPict from '../../assets/image/avatarLand.svg';
 
 const CubeIcon = (props) => <Icon name="cube" {...props} />;
 
-const useInputState = (initialValue = '') => {
-  const [value, setValue] = React.useState(initialValue);
-  return {value, onChangeText: setValue};
-};
-
 const SignUpScreen = ({navigation}) => {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <AvatarsPict width="200" height="140" />
-        </View>
-        <View style={{padding: 20}}>
-          <Input
-            label="Nama Toko"
-            style={styles.input}
-            size="large"
-            placeholder="ex : Toko Mawar Merah"
-          />
-          <Input
-            label="Pemilik"
-            style={styles.input}
-            size="large"
-            placeholder="ex : Budi Mulya"
-          />
-          <Input
-            label="Deskripsi toko anda"
-            multiline={true}
-            style={styles.input}
-            textStyle={{minHeight: 120}}
-            placeholder="Menjual beraneka macam kebutuhan rumah dibidang properti"
-          />
-          <Button
-            onPress={() => navigation.navigate('Dashboard')}
-            size="giant"
-            style={styles.signInButton}
-            accessoryLeft={CubeIcon}>
-            Lanjut
-          </Button>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <AvatarsPict width="200" height="140" />
       </View>
-    </ScrollView>
+      <Input
+        label="Nama Toko"
+        style={styles.input}
+        size="large"
+        placeholder="ex : Toko Mawar Merah"
+      />
+      <Input
+        label="Pemilik"
+        style={styles.input}
+        size="large"
+        placeholder="ex : Budi Mulya"
+      />
+      <Input
+        label="Deskripsi toko anda"
+        multiline={true}
+        style={styles.input2}
+        textStyle={{minHeight: 120}}
+        placeholder="Menjual beraneka macam kebutuhan rumah dibidang properti"
+      />
+      <Button
+        onPress={() => navigation.navigate('Dashboard')}
+        size="giant"
+        style={styles.signInButton}
+        accessoryLeft={CubeIcon}>
+        Lanjut
+      </Button>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  headerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#25CB5C',
+    height: '30%',
+  },
+  avatar: {
+    alignItems: 'center',
+  },
+});
 
 export default SignUpScreen;
